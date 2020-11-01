a9f96afda00ace6ee7f14e4ef8f55017ba9ff5e2
John Edwards <jodoedjr@gmail.com>
2020-11-01 11:32:11 -0500

    Updates

--- src/components/Mailto.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Mailto = ({ email, subject, body, children }) => {
-    return (
-        <a href={`mailto:${email}?subject=${encodeURIComponent(subject) || ''}&body=${encodeURIComponent(body) || ''}`}>{children}</a>
-    );
-};
-
-export default Mailto;
